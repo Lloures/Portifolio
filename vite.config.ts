376622b1235b5bd94c55e1b6a83fb9ca430f9464
Lloures <louresluiza6776@gmail.com>
2025-07-16 16:48:25 -0300

    c

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   plugins: [react(), glsl()],
+  server: {
+    allowedHosts: [
+      'luiza.henriquesoares.dev.br',
+      'lloures.com.br'
+    ]
+  }
 });
